15b3156891250e1778f33db1276e1a749b9ffe16
alexpenedo <alexpenedo@arch>
2014-05-27 12:41:01 +0200

    función escape en el menú principal

--- src/main/java/es/classone/restaurant/web/pages/Index.java
+++ src/main/java/es/classone/restaurant/web/pages/Index.java
@@ -1,9 +1,72 @@
 package es.classone.restaurant.web.pages;
 
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.json.JSONObject;
+import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+
 import es.classone.restaurant.web.services.AuthenticationPolicy;
 import es.classone.restaurant.web.services.AuthenticationPolicyType;
 
 @AuthenticationPolicy(AuthenticationPolicyType.AUTHENTICATED_USERS)
+@Import(library = { "context:javaScript/menu.js" })
 public class Index {
 
+	@Inject
+	private Messages messages;
+
+	@Inject
+	private JavaScriptSupport javaScriptSupport;
+
+	void afterRender() {
+		javaScriptSupport.addInitializerCall(
+				"initMenu",
+				new JSONObject().put("menu", messages.get("menuprincipal"))
+						.put("opcion1", messages.get("opcion1"))
+						.put("opcion2", messages.get("opcion2"))
+						.put("opcion3", messages.get("opcion3"))
+						.put("opcion4", messages.get("opcion4"))
+						.put("opcion5", messages.get("opcion5"))
+						.put("opcion6", messages.get("opcion6"))
+						.put("opcion7", messages.get("opcion7"))
+						.put("opcion8", messages.get("opcion8"))
+						.put("opcion9", messages.get("opcion9"))
+						.put("opciona", messages.get("opciona"))
+						.put("opcionb", messages.get("opcionb"))
+						.put("opcionc", messages.get("opcionc"))
+						.put("opciond", messages.get("opciond"))
+						.put("opcione", messages.get("opcione"))
+						.put("opcionf", messages.get("opcionf"))
+						.put("opciong", messages.get("opciong"))
+						.put("opcionh", messages.get("opcionh"))
+						.put("opcioni", messages.get("opcioni"))
+						.put("opcion11", messages.get("opcion11"))
+						.put("opcion12", messages.get("opcion12"))
+						.put("opcion13", messages.get("opcion13"))
+						.put("opcion14", messages.get("opcion14"))
+						.put("opcion15", messages.get("opcion15"))
+						.put("opcion16", messages.get("opcion16"))
+						.put("opcion17", messages.get("opcion17"))
+						.put("opcion18", messages.get("opcion18"))
+						.put("opcion19", messages.get("opcion19"))
+						.put("opcion1a", messages.get("opcion1a"))
+						.put("opcion1b", messages.get("opcion1b"))
+						.put("opcion1c", messages.get("opcion1c"))
+						.put("opcion1d", messages.get("opcion1d"))
+						.put("opcion1e", messages.get("opcion1e"))
+						.put("opcion1f", messages.get("opcion1f"))
+						.put("opcion1g", messages.get("opcion1g"))
+						.put("opcion1h", messages.get("opcion1h"))
+						.put("opcion1i", messages.get("opcion1i"))
+						.put("opcion21", messages.get("opcion21"))
+						.put("opcion22", messages.get("opcion22"))
+						.put("opcion23", messages.get("opcion23"))
+						.put("opcion24", messages.get("opcion24"))
+						.put("opcion25", messages.get("opcion25"))
+						.put("opcion26", messages.get("opcion26"))
+						.put("opcion27", messages.get("opcion27"))
+						.put("opcion28", messages.get("opcion28"))
+						.put("opcion29", messages.get("opcion29")));
+	}
 }
--- src/main/webapp/javaScript/menu.js
+++ src/main/webapp/javaScript/menu.js
@@ -1,55 +1,172 @@
-var menuBehaviour = function(e){
+var changeLiterals = function(value,spec){
+	if (value==0){
+		$("#button1txt").text(spec.opcion1);
+		$("#button2txt").text(spec.opcion2);
+		$("#button3txt").text(spec.opcion3);
+		$("#button4txt").text(spec.opcion4);
+		$("#button5txt").text(spec.opcion5);
+		$("#button6txt").text(spec.opcion6);
+		$("#button7txt").text(spec.opcion7);
+		$("#button8txt").text(spec.opcion8);
+		$("#button9txt").text(spec.opcion9);
+		$("#buttonAtxt").text(spec.opciona);
+		$("#buttonBtxt").text(spec.opcionb);
+		$("#buttonCtxt").text(spec.opcionc);
+		$("#buttonDtxt").text(spec.opciond);
+		$("#buttonEtxt").text(spec.opcione);
+		$("#buttonFtxt").text(spec.opcionf);
+		$("#buttonGtxt").text(spec.opciong);
+		$("#buttonHtxt").text(spec.opcionh);
+		$("#buttonItxt").text(spec.opcioni);
+		$("#disablemenu").replaceWith( "<li>"+spec.menu+"</li>");
+		$( ".active" ).remove();	
+		$("#headernav ol li").attr("id","activemenu");
+		$("#activemenu").attr("class","active");
+		$("#activemenu").attr("name","menu");
+		$("#buttonmenu").text(spec.menu);
+		
+	}
+	if (value==1){
+	  	$("#button1txt").text(spec.opcion11);
+		$("#button2txt").text(spec.opcion12);
+		$("#button3txt").text(spec.opcion13);
+		$("#button4txt").text(spec.opcion14);
+		$("#button5txt").text(spec.opcion15);
+		$("#button6txt").text(spec.opcion16);
+		$("#button7txt").text(spec.opcion17);
+		$("#button8txt").text(spec.opcion18);
+		$("#button9txt").text(spec.opcion19);
+		$("#buttonAtxt").text("");
+		$("#buttonBtxt").text("");
+		$("#buttonCtxt").text("");
+		$("#buttonDtxt").text("");
+		$("#buttonEtxt").text("");
+		$("#buttonFtxt").text("");
+		$("#buttonGtxt").text("");
+		$("#buttonHtxt").text("");
+		$("#buttonItxt").text("");
+		$("#activemenu").replaceWith( "<li><a>"+spec.menu+"</a></li>");
+		$("#headernav ol li a").attr("href","/restaurant");
+		$("#headernav ol li").attr("id","disablemenu");
+		$("#headernav ol").append("<li>"+spec.opcion1+"</li>");
+		$("#headernav ol li").attr("class","active");
+		$("#disablemenu").attr("name","menu");
+		$("#disablemenu").attr("class","disable");
+		$("#buttonmenu").text(spec.opcion1);
+	}
+	if (value==2){
+		$("#button1txt").text(spec.opcion21);
+		$("#button2txt").text(spec.opcion22);
+		$("#button3txt").text(spec.opcion23);
+		$("#button4txt").text(spec.opcion24);
+		$("#button5txt").text(spec.opcion25);
+		$("#button6txt").text(spec.opcion26);
+		$("#button7txt").text(spec.opcion27);
+		$("#button8txt").text(spec.opcion28);
+		$("#button9txt").text(spec.opcion29);
+		$("#buttonAtxt").text("");
+		$("#buttonBtxt").text("");
+		$("#buttonCtxt").text("");
+		$("#buttonDtxt").text("");
+		$("#buttonEtxt").text("");
+		$("#buttonFtxt").text("");
+		$("#buttonGtxt").text("");
+		$("#buttonHtxt").text("");
+		$("#buttonItxt").text("");
+		$("#activemenu").replaceWith( "<li><a>"+spec.menu+"</a></li>");
+		$("#headernav ol li a").attr("href","/restaurant");
+		$("#headernav ol li").attr("id","disablemenu");
+		$("#headernav ol").append("<li>"+spec.opcion2+"</li>");
+		$("#headernav ol li").attr("class","active");
+		$("#disablemenu").attr("name","menu");
+		$("#disablemenu").attr("class","disable");
+		$("#buttonmenu").text(spec.opcion2);
+	}
+		if (value==3){
+		$("#button1txt").text(spec.opcion31);
+		$("#button2txt").text(spec.opcion32);
+		$("#button3txt").text(spec.opcion33);
+		$("#button4txt").text(spec.opcion34);
+		$("#button5txt").text(spec.opcion35);
+		$("#button6txt").text(spec.opcion36);
+		$("#button7txt").text(spec.opcion37);
+		$("#button8txt").text(spec.opcion38);
+		$("#button9txt").text(spec.opcion39);
+		$("#buttonAtxt").text("");
+		$("#buttonBtxt").text("");
+		$("#buttonCtxt").text("");
+		$("#buttonDtxt").text("");
+		$("#buttonEtxt").text("");
+		$("#buttonFtxt").text("");
+		$("#buttonGtxt").text("");
+		$("#buttonHtxt").text("");
+		$("#buttonItxt").text("");
+		$("#activemenu").replaceWith( "<li><a>"+spec.menu+"</a></li>");
+		$("#headernav ol li a").attr("href","/restaurant");
+		$("#headernav ol li").attr("id","disablemenu");
+		$("#headernav ol").append("<li>"+spec.opcion3+"</li>");
+		$("#headernav ol li").attr("class","active");
+		$("#disablemenu").attr("name","menu");
+		$("#disablemenu").attr("class","disable");
+		$("#buttonmenu").text(spec.opcion3);		
+	}
+	if (value==4){
+		$("#button1txt").text(spec.opcion41);
+		$("#button2txt").text(spec.opcion42);
+		$("#button3txt").text(spec.opcion43);
+		$("#button4txt").text(spec.opcion44);
+		$("#button5txt").text(spec.opcion45);
+		$("#button6txt").text(spec.opcion46);
+		$("#button7txt").text(spec.opcion47);
+		$("#button8txt").text(spec.opcion48);
+		$("#button9txt").text(spec.opcion49);
+		$("#buttonAtxt").text("");
+		$("#buttonBtxt").text("");
+		$("#buttonCtxt").text("");
+		$("#buttonDtxt").text("");
+		$("#buttonEtxt").text("");
+		$("#buttonFtxt").text("");
+		$("#buttonGtxt").text("");
+		$("#buttonHtxt").text("");
+		$("#buttonItxt").text("");
+		$("#activemenu").replaceWith( "<li><a>"+spec.menu+"</a></li>");
+		$("#headernav ol li a").attr("href","/restaurant");
+		$("#headernav ol li").attr("id","disablemenu");
+		$("#headernav ol").append("<li>"+spec.opcion4+"</li>");
+		$("#headernav ol li").attr("class","active");
+		$("#disablemenu").attr("name","menu");
+		$("#disablemenu").attr("class","disable");
+		$("#buttonmenu").text(spec.opcion4);		
+	}		 		 
+}
+
+
+var menuBehaviour = function(e,spec){
 		e.preventDefault();
-		console.log(e.which + ": " +
-		String.fromCharCode(e.which));
-		var currentOption=$('#activemenu').attr('name');
+		console.log(e.which + ": " +String.fromCharCode(e.which));
+		var currentOption=$('.active').attr('name');
 		console.log(currentOption);
 		if((String.fromCharCode(e.which)==1)&&(currentOption=="menu")){
-			$("#button1txt").text("${message:opcion11}");
-			$("#button2txt").text("${message:opcion12}");
-			$("#button3txt").text("${message:opcion13}");
-			$("#button4txt").text("${message:opcion14}");
-			$("#button5txt").text("${message:opcion15}");
-			$("#button6txt").text("${message:opcion16}");
-			$("#button7txt").text("${message:opcion17}");
-			$("#button8txt").text("${message:opcion18}");
-			$("#button9txt").text("${message:opcion19}");
-			$("#buttonAtxt").text("");
-			$("#buttonBtxt").text("");
-			$("#buttonCtxt").text("");
-			$("#buttonDtxt").text("");
-			$("#buttonEtxt").text("");
-			$("#buttonFtxt").text("");
-			$("#buttonGtxt").text("");
-			$("#buttonHtxt").text("");
-			$("#buttonItxt").text("");
-			$("#activemenu").replaceWith( "<li><a>${message:menuprincipal}</a></li>");
-			$("#headernav ol li a").attr("href","/restaurant");
-			$("#headernav ol").append("<li>${message:opcion1}</li>");
-			$("#headernav ol li").attr("class","active");
+			changeLiterals(1,spec);
 		  }
 		  if((String.fromCharCode(e.which)==2)&&(currentOption=="menu")){
-			$("#button2txt").text("${message:opcion21}");
-			$("#button2txt").text("${message:opcion22}");
-			$("#button3txt").text("${message:opcion23}");
-			$("#button4txt").text("${message:opcion24}");
-			$("#button5txt").text("${message:opcion25}");
-			$("#button6txt").text("${message:opcion26}");
-			$("#button7txt").text("${message:opcion27}");
-			$("#button8txt").text("${message:opcion28}");
-			$("#button9txt").text("");
-			$("#buttonAtxt").text("");
-			$("#buttonBtxt").text("");
-			$("#buttonCtxt").text("");
-			$("#buttonDtxt").text("");
-			$("#buttonEtxt").text("");
-			$("#buttonFtxt").text("");
-			$("#buttonGtxt").text("");
-			$("#buttonHtxt").text("");
-			$("#buttonItxt").text("");
-			$("#activemenu").replaceWith( "<li><a>${message:menuprincipal}</a></li>");
-			$("#headernav ol li a").attr("href","/restaurant");
-			$("#headernav ol").append("<li>${message:opcion2}</li>");
-			$("#headernav ol li").attr("class","active");
+			changeLiterals(2,spec);
 		  }
 		};
+		
+		
+(function( $, window ) {		
+	 'use scrict';  
+	  $.extend(Tapestry.Initializer, {
+	 	 initMenu: function (spec) {
+	 	 $("#menuexperto").keypress(function(e){
+ 			menuBehaviour(e,spec);	
+		 });
+		 $(document).keyup(function(e) {
+  				if (e.keyCode == 27) { 
+  				if ($("#activemenu").attr("name")!="menu")
+  				changeLiterals(0,spec);}  
+		 });
+	    }
+	   })
+})(jQuery, window);
